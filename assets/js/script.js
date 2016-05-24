f14ed09dcea463b75130e3cc49a8cc252785eba3
manuKunam <emmanuel.kormann@gmail.com>
2016-05-24 15:10:15 +0200

    hello
    c’est moi

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ L.mapbox.accessToken = 'pk.eyJ1IjoicGllcnJlcGllcnJlcGllcnJlIiwiYSI6IkdXdE5CRFEif
 
 
 $( document ).ready(function() {
-	
+
 
 	/* UPLOAD FILES INPUT */
 	var inputs = document.querySelectorAll( '.fileupload' );
@@ -28,16 +28,16 @@ $( document ).ready(function() {
 
 				if (file.type.match('image.*')) {
 					// http://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
-					var reader = new FileReader();		
+					var reader = new FileReader();
 					reader.onload = function (e) {
 						$('#inputImg').html('<img src="'+e.target.result+'">');
-					}			
+					}
 					reader.readAsDataURL(this.files[0]);
 				}
-				if (file.type.match('application/pdf') || file.name.match('\.pdf')) { 
+				if (file.type.match('application/pdf') || file.name.match('\.pdf')) {
 					$('#inputImg').html('<span class="gros fichier pdf"></span>');
 				}
-				if (file.type.match('application/msword') || file.type.match('application\/.*officedocument') || file.name.match('\.doc') ) { 
+				if (file.type.match('application/msword') || file.type.match('application\/.*officedocument') || file.name.match('\.doc') ) {
 					$('#inputImg').html('<span class="gros fichier doc"></span>');
 				}
 			} else {
@@ -53,8 +53,11 @@ $( document ).ready(function() {
 		openMenu = false;
 	}
 
-	$(document).on('click', function(e) { 
+	$(document).on('click', function(e) {
+
+		// Pour supprimer les messages d'avertissement des formulaires au clique.
 		if($(document).find('.message.ok').length && !okremoved) $(document).find('.message.ok').remove(); var okremoved = true;
+
 
 		if(openMenu != false){
 			if(!$(e.target).closest('.select-menu').length){
@@ -75,7 +78,7 @@ $( document ).ready(function() {
 						$menu.find('input').hide();
 					}
 					*/
-					
+
 					$target.addClass('selected');
 					$old.removeClass('selected');
 
@@ -85,7 +88,7 @@ $( document ).ready(function() {
 					closeMenu();
 				}
 			}
-		} 
+		}
 	}).on('click', '.select-title', function(e){
 		if ($(this).hasClass('open')){
 			closeMenu();
@@ -95,16 +98,11 @@ $( document ).ready(function() {
 		}
 	});
 
-	$('.modal').on('click', function(e){
-		window.history.go(-1); 
-		return false;
-	}).on('click', 'section.edition', function(e){
-		e.stopPropagation();		
-	}).on('click', '.bouton.fermer', function(e){
-		window.history.go(-1); 
-		return false;
-	})
 
+
+
+
+	// pour le futur
 	$('.modal').on('click', '.diapositive .supprimer-button', function(){
 		$(this).closest('.diapositive').addClass('deletemode');
 	}).on('click', '.diapositive .bouton.annuler', function(){
@@ -116,8 +114,6 @@ $( document ).ready(function() {
 	}).on('click', '.diapositive.ajout .bouton.annuler', function(){
 		$(this).closest('.diapositive').removeClass('ajout').addClass('fake');
 	});
-
-
 
 
 
@@ -158,40 +154,43 @@ $( document ).ready(function() {
 		}
 	});
 
-	
+
 	/* Ancres dans les tabs */
 	$(".thematique-sommaire a").click(function(e) {
 		e.preventDefault();
-		var dest = $(this).attr('href'); 
+		var dest = $(this).attr('href');
 		$('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow');
 	})
-	
 
-	
+
+
 	/* ----- gestion des onglets (tab) ---- */
-	
-	
-	$("#nav-tabs li a").click(function(e) {
-		e.preventDefault();
-		history.pushState({ path: this.path }, '', this.href);
-		var $inventaireMenu = $(this).closest('#nav-tabs');        
-		if (!$(this).hasClass('chapitre') && !$(this).hasClass('bouton')) {
-			if (!$(this).hasClass('strong') && $(this).siblings('ul.chapitres').find('a.strong').length <= 0) {
-				$inventaireMenu.find('.chapitres').slideUp(300);
-				$(this).next('.chapitres').slideDown(300, function(){
-					console.log('yo');
-					$inventaireMenu.sticky({topSpacing:12});     		
-				});
-			}
-		}
 
-		$(".tabs-wrapper article.tab").hide();
-		document.getElementById( this.getAttribute('data-cible') ).style.display = 'block'; 
-		$inventaireMenu.find("li a").removeClass('strong');
-		$(this).addClass('strong');
-		return false;
-	});
-	
+	if (!$('body').hasClass('logged')) {
+
+		$("#nav-tabs li a").click(function(e) {
+			e.preventDefault();
+			history.pushState({ path: this.path }, '', this.href);
+			var $inventaireMenu = $(this).closest('#nav-tabs');
+			if (!$(this).hasClass('chapitre') && !$(this).hasClass('bouton')) {
+				if (!$(this).hasClass('strong') && $(this).siblings('ul.chapitres').find('a.strong').length <= 0) {
+					$inventaireMenu.find('.chapitres').slideUp(300);
+					$(this).next('.chapitres').slideDown(300, function(){
+						//$inventaireMenu.sticky({topSpacing:12});
+					});
+				}
+			}
+
+			$(".tabs-wrapper article.tab").hide();
+			if (document.getElementById( this.getAttribute('data-cible') )){
+				document.getElementById( this.getAttribute('data-cible') ).style.display = 'block';
+			}
+			$inventaireMenu.find("li a").removeClass('strong');
+			$(this).addClass('strong');
+			return false;
+		});
+	}
+
 	/* Qu'est ce ce truc ? pas simplifiable juste pur css ? */
 	if ($('#nav-tabs').length > 0) {
 		var $inventaireMenu = $('#nav-tabs');
@@ -215,10 +214,9 @@ $( document ).ready(function() {
 	// pour afficher le premier article si aucun n'est ouvert. J'ai pas réussi à le faire en SPIP...
 	if ($('#nav-tabs').length > 0) {
 		if($('#nav-tabs').find('a.strong').length <= 0){
-			console.log('yol');
 			$('#nav-tabs').find('li a').first().trigger('click');
 		} else {
-			$('#nav-tabs').sticky({topSpacing:12});     		
+			// $('#nav-tabs').sticky({topSpacing:12});
 
 		}
 	}
@@ -226,15 +224,15 @@ $( document ).ready(function() {
 
 	/* ----- / fin gestion des onglets (tab) ---- */
 
-	
+
 	/* Init Mapbox */
 	if($("#welcome-map").length > 0) {
 		var map = L.mapbox.map('welcome-map', 'mapbox.streets', {zoomControl: false })
 	.setView([37, 16.5], 5);
 		map.scrollWheelZoom.disable();
 	}
-	
-	
+
+
 	if($("#inventaire-mapbox").length > 0){
 		var map = L.mapbox.map('inventaire-mapbox', 'mapbox.streets').setView([37, 16.5], 5);
 			map.scrollWheelZoom.disable();
@@ -243,10 +241,10 @@ $( document ).ready(function() {
 
 			var markers = L.mapbox.featureLayer()
 				.setGeoJSON(geojson)
-				.addTo(map);	
+				.addTo(map);
 
 			map.fitBounds(markers.getBounds(), { padding: L.point(60, 60), maxZoom: maxZoom });
-			
+
 			markers.on('click', function(e) {
 				if (!$("#inventaire-carte").hasClass('projet')) {
 					window.location = e.layer.feature.properties['url'];
